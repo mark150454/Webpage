bc169178c8131ad2731ba690621ad2d19a09f842
lukejack <jacksonluke96@yahoo.co.uk>
2016-03-16 13:04:16 +0000

    Added files via upload

---
+++ boardconfig.js
@@ -0,0 +1,113 @@
+engine = new Worker('stockfish.js');
+playerSide = ' w';
+bestData = 'NONE';
+engineMessages = [];
+
+$(document).ready(function(){
+    init();
+    document.getElementById("ask").addEventListener("click", bestMove);
+    document.getElementById("show").addEventListener("click", show);
+});
+
+var init = function() {
+            var board,
+  game = new Chess(),
+  statusEl = $('#status'),
+  fenEl = $('#fen'),
+  pgnEl = $('#pgn');
+
+// do not pick up pieces if the game is over
+// only pick up pieces for the side to move
+var onDragStart = function(source, piece, position, orientation) {
+  if (game.game_over() === true ||
+      (game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+      (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+    return false;
+  }
+};
+
+var onDrop = function(source, target) {
+  // see if the move is legal
+  var move = game.move({
+    from: source,
+    to: target,
+    promotion: 'q' // NOTE: always promote to a queen for example simplicity
+  });
+
+  // illegal move
+  if (move === null) return 'snapback';
+
+  updateStatus();
+};
+
+// update the board position after the piece snap 
+// for castling, en passant, pawn promotion
+var onSnapEnd = function() {
+  board1.position(game.fen());
+};
+
+var updateStatus = function() {
+  var status = '';
+
+  var moveColor = 'White';
+  if (game.turn() === 'b') {
+    moveColor = 'Black';
+  }
+
+  // checkmate?
+  if (game.in_checkmate() === true) {
+    status = 'Game over, ' + moveColor + ' is in checkmate.';
+  }
+
+  // draw?
+  else if (game.in_draw() === true) {
+    status = 'Game over, drawn position';
+  }
+
+  // game still on
+  else {
+    status = moveColor + ' to move';
+
+    // check?
+    if (game.in_check() === true) {
+      status += ', ' + moveColor + ' is in check';
+    }
+  }
+
+  statusEl.html(status);
+  fenEl.html(game.fen());
+  pgnEl.html(game.pgn());
+};
+
+var cfg = {
+  draggable: true,
+  position: 'start',
+  onDragStart: onDragStart,
+  onDrop: onDrop,
+  onSnapEnd: onSnapEnd
+};
+board1 = ChessBoard('board', cfg);
+
+updateStatus();
+        }; // end init()
+
+function bestMove() {
+    engine.postMessage('position fen ' + board1.fen() + playerSide);
+    engine.postMessage('go movetime 10');
+};
+
+engine.onmessage = function(event) {
+    //var son = JSON.stringify(event.data);
+    //var jsonArray = eval('(' + son + ')');
+    //console.log('1');
+    //console.log(jsonArray);
+    engineMessages.push(String(event.data));
+};
+
+function show() {
+	
+
+	//Get the last line
+	console.log(engineMessages[engineMessages.length - 1]);
+};
+
